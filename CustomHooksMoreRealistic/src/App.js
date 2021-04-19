8e9019045c3b9861107251abdda6102d2d259df8
Dima <webbootcamp2020@gmail.com>
2021-04-19 22:19:00 +0300

    Custom hooks

---
+++ CustomHooksMoreRealistic/src/App.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import useHttp from "./hooks/use-http";
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const transformTasks = (tasksObj) => {
+    const loadedTasks = [];
+
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+    }
+  };
+
+  const { isLoading, error, sendRequest } = useHttp(
+    {
+      url: "https://react-http-7e725-default-rtdb.firebaseio.com/tasks.json",
+    },
+    transformTasks
+  );
+
+  useEffect(() => {
+    sendRequest();
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={sendRequest}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
